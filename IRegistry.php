9a8f90580b645f07e9110a340b674f2602e08aff
Greg Beaver <greg@chiaraquartet.net>
2007-06-06 18:03:52 +0000

    initial import of Pyrus (unchanged from pear-core/PEAR at cvs.php.net)
    git-svn-id: http://svn.php.net/repository/pear2/pyrus/trunk@284753 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ IRegistry.php
@@ -0,0 +1,18 @@
+<?php
+interface PEAR2_IRegistry
+{
+    public function installPackage(PEAR2_PackageFile_v2 $info);
+    public function upgradePackage(PEAR2_PackageFile_v2 $info);
+    public function uninstallPackage($name, $channel);
+    public function addChannel(PEAR2_ChannelFile $channel);
+    public function updateChannel(PEAR2_ChannelFile $channel);
+    public function deleteChannel($channel);
+    public function packageExists($package, $channel);
+    public function channelAlias($channel);
+    public function channelExists($channel, $strict = true);
+    public function hasMirror($channel, $mirror);
+    public function setAlias($channel, $alias);
+    public function getMirrors($channel);
+    public function packageInfo($package, $channel, $field);
+    public function __get($var);
+}
---
+++ Registry/Package.php
@@ -0,0 +1,54 @@
+<?php
+class PEAR2_Registry_Package implements ArrayAccess
+{
+    private $_packagename;
+    private $_reg;
+    function __construct(PEAR2_IRegistry $cloner)
+    {
+        $this->_reg = $cloner;
+    }
+
+    function offsetExists($offset)
+    {
+ 	    $info = $this->_reg->parsePackageName($offset);
+        return $this->_reg->packageExists($info['package'], $info['channel']);
+    }
+
+    function offsetGet($offset)
+ 	{
+ 	    $this->_packagename = $this->_reg->parsePackageName($this->_packagename);
+ 	    $ret = clone $this;
+ 	    return $ret;
+ 	}
+ 	
+ 	function offsetSet($offset, $value)
+ 	{
+ 	    if ($offset == 'upgrade') {
+ 	        $this->_reg->upgradePackage($value);
+ 	    }
+ 	    if ($offset == 'install') {
+ 	        $this->_reg->installPackage($value);
+ 	    }
+ 	}
+
+ 	function offsetUnset($offset)
+ 	{
+ 	    $info = $this->parsePackageName($offset);
+ 	    $this->uninstallPackage($info['package'], $info['channel']);
+ 	}
+
+ 	function __get($var)
+ 	{
+ 	    if (!isset($this->_packagename)) {
+ 	        throw new PEAR2_Registry_Exception('Attempt to retrieve ' . $var .
+                ' from unknown package');
+ 	    }
+ 	    return $this->_reg->packageInfo($this->_packagename['package'], 
+ 	      $this->_packagename['channel'], $var);
+ 	}
+
+ 	function __call($method, $args)
+ 	{
+ 	    return call_user_func_array(array($this->_reg, $method), $args);
+ 	}
+}
