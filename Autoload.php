8ac3f3038b2795bb6e42567653cfa30c3750b7c3
Greg Beaver <greg@chiaraquartet.net>
2007-09-03 23:54:47 +0000

    remove weird simulation of include_once, add auto-declaration of __autoload() if it doesn't already exist
    git-svn-id: http://svn.php.net/repository/pear2/Pyrus/trunk@284855 c90b9560-bf6c-de11-be94-00142212c4b1

--- Autoload.php
+++ Autoload.php
@@ -1,20 +1,19 @@
 <?php
-if (function_exists('PEAR2_Autoload')) {
-    return;
-} else {
-    function PEAR2_Autoload($class)
-    {
-        if (substr($class, 0, 6) !== 'PEAR2_') {
-            return false;
-        }
-        $fp = @fopen(str_replace('_', '/', $class) . '.php', 'r', true);
-        if ($fp) {
-            fclose($fp);
-            require str_replace('_', '/', $class) . '.php';
-            return true;
-        }
-        throw new Exception('Class $class could not be loaded from ' .
-            str_replace('_', '/', $class) . '.php (include_path="' . get_include_path() .
-            '")');
+function PEAR2_Autoload($class)
+{
+    if (substr($class, 0, 6) !== 'PEAR2_') {
+        return false;
     }
+    $fp = @fopen(str_replace('_', '/', $class) . '.php', 'r', true);
+    if ($fp) {
+        fclose($fp);
+        require str_replace('_', '/', $class) . '.php';
+        return true;
+    }
+    throw new Exception('Class $class could not be loaded from ' .
+        str_replace('_', '/', $class) . '.php (include_path="' . get_include_path() .
+        '")');
+}
+if (!function_exists('__autoload')) {
+    function __autoload($class) { return PEAR2_Autoload($class); }
 }
