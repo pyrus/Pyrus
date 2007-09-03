090461af97cf0143ae7ed3666da3ebe64563761b
Greg Beaver <greg@chiaraquartet.net>
2007-09-03 22:57:33 +0000

    add PEAR2_Autoload_strict()
    git-svn-id: http://svn.php.net/repository/pear2/Pyrus/trunk@284853 c90b9560-bf6c-de11-be94-00142212c4b1

--- Autoload.php
+++ Autoload.php
@@ -12,4 +12,18 @@ if (function_exists('PEAR2_Autoload')) {
         }
         return false;
     }
+
+    function PEAR2_Autoload_strict($class)
+    {
+        if (substr($class, 0, 6) !== 'PEAR2_') {
+            return false;
+        }
+        $fp = @fopen(str_replace('_', '/', $class) . '.php', 'r', true);
+        if ($fp) {
+            fclose($fp);
+            require str_replace('_', '/', $class) . '.php';
+            return true;
+        }
+        return false;
+    }
 }
