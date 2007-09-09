9e05685b1c70456ba66971ef0d23888fae439a62
Greg Beaver <greg@chiaraquartet.net>
2007-09-09 17:38:55 +0000

    1) after loading the file that should contain the class, an extra check is added to ensure that the class indeed exists, and an exception is thrown if this is not the case to assist in debugging. 2) if SPL is loaded, spl_autoload_functions is checked for the presence of PEAR2_Autoload, and is registered.  if __autoload exists, it is also registered if no spl_autoload stack was active. 3) all global variables used are prepended with a bunch of ____ to make sure they don't conflict with any that may exist in the user code, and are unset at the end of the file.
    git-svn-id: http://svn.php.net/repository/pear2/Pyrus/trunk@284881 c90b9560-bf6c-de11-be94-00142212c4b1

--- src/Autoload.php
+++ src/Autoload.php
@@ -8,23 +8,45 @@ function PEAR2_Autoload($class)
     if ($fp) {
         fclose($fp);
         require str_replace('_', '/', $class) . '.php';
+        if (!class_exists($class, false)) {
+            throw new Exception('Class ' . $class . ' was not present in ' .
+                str_replace('_', '/', $class) . '.php (include_path="' . get_include_path() .
+                '") [PEAR2_Autoload version 1.0]');
+        }
         return true;
     }
-    throw new Exception('Class $class could not be loaded from ' .
+    throw new Exception('Class ' . $class . ' could not be loaded from ' .
         str_replace('_', '/', $class) . '.php (include_path="' . get_include_path() .
-        '") [autoload version 1.0]');
+        '") [PEAR2_Autoload version 1.0]');
 }
-if (!function_exists('__autoload')) {
+
+// set up __autoload
+if (function_exists('spl_autoload_register')) {
+    if (!($_____t = spl_autoload_functions()) || !in_array('PEAR2_Autoload', spl_autoload_functions())) {
+        spl_autoload_register('PEAR2_Autoload');
+        if (function_exists('__autoload') && ($_____t === false)) {
+            // __autoload() was being used, but now would be ignored, add
+            // it to the autoload stack
+            spl_autoload_register('__autoload');
+        }
+    }
+    unset($_____t);
+} else {
     function __autoload($class) { return PEAR2_Autoload($class); }
 }
-$paths = explode(PATH_SEPARATOR, get_include_path());
-$found = false;
-foreach ($paths as $path) {
-    if ($path == dirname(dirname(__FILE__))) {
-        $found = true;
+
+// set up include_path if it doesn't register our current location
+$____paths = explode(PATH_SEPARATOR, get_include_path());
+$____found = false;
+foreach ($____paths as $____path) {
+    if ($____path == dirname(dirname(__FILE__))) {
+        $____found = true;
         break;
     }
 }
-if (!$found) {
+if (!$____found) {
     set_include_path(get_include_path() . PATH_SEPARATOR . dirname(dirname(__FILE__)));
 }
+unset($____paths);
+unset($____path);
+unset($____found);
