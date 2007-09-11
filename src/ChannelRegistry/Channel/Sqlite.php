3f55f3b11466683273ab991112db5a221a346110
Greg Beaver <greg@chiaraquartet.net>
2007-09-11 22:52:19 +0000

    correct signature of creation of sqlite mirror
    git-svn-id: http://svn.php.net/repository/pear2/Pyrus/trunk@284897 c90b9560-bf6c-de11-be94-00142212c4b1

--- src/ChannelRegistry/Channel/Sqlite.php
+++ src/ChannelRegistry/Channel/Sqlite.php
@@ -92,8 +92,8 @@ class PEAR2_Pyrus_ChannelRegistry_Channel_Sqlite implements PEAR2_Pyrus_IChannel
  	            foreach ($this->database->arrayQuery('SELECT server FROM channel_servers
  	                  WHERE channel=\'' . sqlite_escape_string($this->_channelname) . '\'
  	                  AND server !=\'' . sqlite_escape_string($this->_channelname) . '\'') as $mirror) {
- 	                $ret[$mirror] = new PEAR2_Pyrus_ChannelRegistry_Mirror_Sqlite($this->database,
- 	                    $this->_channelname, $mirror);
+ 	                $ret[$mirror] = new PEAR2_Pyrus_ChannelRegistry_Mirror_Sqlite($this,
+ 	                                    $this->database, $mirror);
                 }
  	    }
  	}
