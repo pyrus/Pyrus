06b5eb72820cc298905551c9aef079c0cc6778c2
Greg Beaver <greg@chiaraquartet.net>
2007-09-26 19:35:26 +0000

    test file used on windows
    git-svn-id: http://svn.php.net/repository/pear2/Pyrus/trunk@284951 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ src/test2.php
@@ -0,0 +1,56 @@
+<?php
+function __autoload($class)
+{
+    if (substr($class, 0, 4) != 'PEAR') return false;
+    $path = explode('_', substr($class, 11)); // strip PEAR2_Pyrus for CVS
+    $path = dirname(__FILE__) . implode('\\', $path) . '.php';
+    include $path;
+}
+set_include_path('C:/development/PEAR2/Pyrus/src');
+include $a = 'C:/development/PEAR2/Exception/Exception.php';
+include $b = 'C:/development/PEAR2/MultiErrors/MultiErrors.php';
+include 'C:/development/PEAR2/MultiErrors/MultiErrors/Exception.php';
+include '/home/cellog/workspace/PEAR2/Pyrus_Developer/Creator/Zip.php';
+include '/home/cellog/workspace/PEAR2/Pyrus_Developer/Creator/Tar.php';
+include '/home/cellog/workspace/PEAR2/Pyrus_Developer/Creator/Xml.php';
+include '/home/cellog/workspace/PEAR2/Pyrus_Developer/Creator/Exception.php';
+$a = new PEAR2_Pyrus_Package_Creator(array(
+        new PEAR2_Pyrus_Developer_Creator_Zip('C:/development/PEAR2/blah.zip'),
+        new PEAR2_Pyrus_Developer_Creator_Tar('C:/development/PEAR2/blah.tgz'),
+        new PEAR2_Pyrus_Developer_Creator_Xml('C:/development/PEAR2/blah.xml'),
+    ), $a, 'C:/development/PEAR2/Autoload/Autoload.php', $b);
+$b = new PEAR2_Pyrus_Package('C:/development/pear-core/PEAR-1.6.0.tgz');
+$a->render($b);
+exit;
+$pf = new PEAR2_Pyrus_PackageFile_v2;
+//$pf->name = 'test';
+//$pf->channel = 'pear.php.net';
+//$pf->summary = 'test';
+//$pf->description = 'testing';
+//$pf->maintainer['cellog']->name('Greg Beaver')->role('lead')->email('cellog@php.net')
+//    ->active('yes');
+//$a = new DateTime();
+//$pf->date = $a->format('Y-m-d');
+//$pf->version['release'] = '1.0.0';
+//$pf->version['api'] = '1.0.0';
+//$pf->stability['release'] = 'stable';
+//$pf->stability['api'] = 'stable';
+//$pf->license = 'PHP License';
+//$pf->notes = 'test';
+//$pf->dependencies->required->php = array('min' => '5.2.0');
+//$pf->dependencies->required->pearinstaller = array('min' => '5.2.0', 'exclude' => '1.2.3');
+//$pf->files['test/me.php'] = array('attribs' => array('role' => 'php'));
+//$pf = new PEAR2_Pyrus_package(dirname(__FILE__) . '/test.xml');
+//foreach ($pf->packagingcontents as $name => $file) {
+//    var_dump($name, $file);
+//}
+//exit;
+define('OS_WINDOWS', false);
+define('OS_UNIX', true);
+$g = new PEAR2_Pyrus_Config('C:/development/pear-core/testpear');
+//$g = new PEAR2_Pyrus_Config('/home/cellog/testpear');
+$g->saveConfig();
+$a = new PEAR2_Pyrus_Package('C:/development/pear-core/PEAR-1.5.0a1.tgz');
+//$a = new PEAR2_Pyrus_Package('/home/cellog/workspace/pear-core/PEAR-1.6.2.tgz');
+$b = new PEAR2_Pyrus_Installer;
+$b->install($a);
