c83d8b4203ca9396e0e1e8738112fd61b47001da
Greg Beaver <greg@chiaraquartet.net>
2008-05-16 22:32:10 +0000

    fix include_path cascade to seek to the correct directory
    git-svn-id: http://svn.php.net/repository/pear2/Pyrus/trunk@285132 c90b9560-bf6c-de11-be94-00142212c4b1

--- src/Pyrus/Config.php
+++ src/Pyrus/Config.php
@@ -298,6 +298,13 @@ class PEAR2_Pyrus_Config
             // are doing, don't add include_path
             PEAR2_Pyrus_Log::log(1, 'Automatically cascading include_path');
             $extra = explode(PATH_SEPARATOR, get_include_path());
+            foreach ($extra as &$path) {
+                if (substr($path, strlen($path) - 3) == 'src' && $path[strlen($path) - 4] == DIRECTORY_SEPARATOR) {
+                    // include_path goes to the php_dir which is always src, so our config
+                    // file is in the parent directory.
+                    $path = dirname($path);
+                }
+            }
             array_unshift($extra, $ret);
             $paths = $extra;
         }
