a62c43dcaadc2423f27f1c8667e1d22f10b89e6f
Brett Bieber <brett.bieber@gmail.com>
2008-05-16 20:55:31 +0000

    Content can be integer as well as string. Fixes broken xml when content of element was an int (return from filesize() for example).
    git-svn-id: http://svn.php.net/repository/pear2/Pyrus/trunk@285124 c90b9560-bf6c-de11-be94-00142212c4b1

--- src/Pyrus/XMLWriter.php
+++ src/Pyrus/XMLWriter.php
@@ -115,7 +115,7 @@ class PEAR2_Pyrus_XMLWriter
                 $this->_writer->startElementNs($ns, $element, $this->_namespaces[$ns]);
             }
         } else {
-            if (is_string($values)) {
+            if (is_string($values) || is_int($values)) {
                 if (strlen($values)) {
                     $this->_writer->writeElement($key, $values);
                 } else {
