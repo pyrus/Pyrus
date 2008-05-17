affad6b2494e75af7f97e652fbd7bde130510606
Helgi Þormar Þorbjörnsson <helgi@php.net>
2008-05-17 20:31:18 +0000

    CS, tabs to spaces
    git-svn-id: http://svn.php.net/repository/pear2/Pyrus/trunk@285147 c90b9560-bf6c-de11-be94-00142212c4b1

--- src/Pyrus/Registry/Sqlite/Package.php
+++ src/Pyrus/Registry/Sqlite/Package.php
@@ -33,38 +33,38 @@ class PEAR2_Pyrus_Registry_Sqlite_Package extends PEAR2_Pyrus_Registry_Sqlite im
 
     function offsetExists($offset)
     {
- 	    $info = PEAR2_Pyrus_Config::current()->channelregistry->parseName($offset);
+        $info = PEAR2_Pyrus_Config::current()->channelregistry->parseName($offset);
         return $this->exists($info['package'], $info['channel']);
     }
 
     function offsetGet($offset)
- 	{
- 	    $this->_packagename = $offset;
- 	    $ret = clone $this;
- 	    unset($this->_packagename);
- 	    return $ret;
- 	}
+    {
+        $this->_packagename = $offset;
+        $ret = clone $this;
+        unset($this->_packagename);
+        return $ret;
+    }
 
- 	function offsetSet($offset, $value)
- 	{
- 	    if ($offset == 'install') {
- 	        $this->install($value);
- 	    }
- 	}
+    function offsetSet($offset, $value)
+    {
+        if ($offset == 'install') {
+            $this->install($value);
+        }
+    }
 
- 	function offsetUnset($offset)
- 	{
- 	    $info = PEAR2_Pyrus_Config::current()->channelregistry->parseName($offset);
- 	    $this->uninstall($info['package'], $info['channel']);
- 	}
+    function offsetUnset($offset)
+    {
+        $info = PEAR2_Pyrus_Config::current()->channelregistry->parseName($offset);
+        $this->uninstall($info['package'], $info['channel']);
+    }
 
- 	function __get($var)
- 	{
- 	    if (!isset($this->_packagename)) {
- 	        throw new PEAR2_Pyrus_Registry_Exception('Attempt to retrieve ' . $var .
+    function __get($var)
+    {
+        if (!isset($this->_packagename)) {
+            throw new PEAR2_Pyrus_Registry_Exception('Attempt to retrieve ' . $var .
                 ' from unknown package');
- 	    }
- 	    $info =  PEAR2_Pyrus_Config::current()->channelregistry->parseName($this->_packagename);
- 	    return $this->info($info['package'], $info['channel'], $var);
- 	}
+        }
+        $info =  PEAR2_Pyrus_Config::current()->channelregistry->parseName($this->_packagename);
+        return $this->info($info['package'], $info['channel'], $var);
+    }
 }
