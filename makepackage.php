8177549966cf746e11eaa08073ebfce7f3a18161
Brett Bieber <brett.bieber@gmail.com>
2008-05-27 13:35:23 +0000

    Add script to generate pyrus.phar from a SVN checkout of 'all' - remove Net_URL2 reference, I couldn't find any references to it.
    git-svn-id: http://svn.php.net/repository/pear2/Pyrus/trunk@285221 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ makepackage.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * This file generates the pyrus.phar file and PEAR2 package for Pyrus.
+ */
+error_reporting(E_ALL);
+ini_set('display_errors',true);
+require_once dirname(__FILE__).'/../autoload.php';
+
+$a = new PEAR2_Pyrus_Developer_PackageFile_PEAR2SVN(dirname(__FILE__), 'PEAR2_Pyrus');
+
+$package = new PEAR2_Pyrus_Package('package.xml');
+$outfile = $package->name.'-'.$package->version['release'];
+$a = new PEAR2_Pyrus_Package_Creator(array(
+                    new PEAR2_Pyrus_Developer_Creator_Phar_PHPArchive(__DIR__ . '/pyrus.phar', '<?php
+function __autoload($class)
+{
+    include \'phar://\' . PYRUS_PHAR_FILE . \'/src/\' . implode(\'/\', explode(\'_\', $class)) . \'.php\';
+}
+$frontend = new PEAR2_Pyrus_ScriptFrontend_Commands;
+@array_shift($_SERVER[\'argv\']);
+$frontend->run($_SERVER[\'argv\']);
+'),),
+                    dirname(__FILE__) . '/../Exception/src/Exception.php',
+					dirname(__FILE__) . '/../Autoload/src/Autoload.php',
+					dirname(__FILE__) . '/../MultiErrors/src/MultiErrors.php');
+$b = new PEAR2_Pyrus_Package(__DIR__ . '/package.xml');
+$rp = __DIR__ . '/../HTTP_Request/src/HTTP';
+$a->render($b, array(
+    'src/PEAR2/HTTP/Request.php' => $rp . '/Request.php',
+    'src/PEAR2/HTTP/Request/Adapter.php' => $rp . '/Request/Adapter.php',
+    'src/PEAR2/HTTP/Request/Adapter/Phpsocket.php' => $rp . '/Request/Adapter/Phpsocket.php',
+    'src/PEAR2/HTTP/Request/Adapter/Phpstream.php' => $rp . '/Request/Adapter/Phpstream.php',
+    'src/PEAR2/HTTP/Request/Exception.php' => $rp . '/Request/Exception.php',
+    'src/PEAR2/HTTP/Request/Headers.php' => $rp . '/Request/Headers.php',
+    'src/PEAR2/HTTP/Request/Response.php' => $rp . '/Request/Response.php',
+    'src/PEAR2/HTTP/Request/Uri.php' => $rp . '/Request/Uri.php',
+));
