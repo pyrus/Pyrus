8a5cf455fa870cf54746cc38aa583ca66e3701bf
Greg Beaver <greg@chiaraquartet.net>
2009-05-18 03:24:33 +0000

    begin infrastructure for testing internet junk, use directory-based system
    git-svn-id: http://svn.php.net/repository/pear2/Pyrus/trunk@285843 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/Mocks/Internet.php
+++ tests/Mocks/Internet.php
@@ -4,7 +4,7 @@
  */
 class Internet extends PEAR2_HTTP_Request
 {
-    public $requestMap;
+    public static $requestMap;
 
     /**
      * sets up the adapter
@@ -19,17 +19,13 @@ class Internet extends PEAR2_HTTP_Request
         }
     }
 
-    function addRequest($verb, $url, $headers, $responseheaders, $body, $code = '200')
+    function addDirectory($dir, $urlbase)
     {
-        $isfile = file_exists($body);
-        $this->requestMap[$verb][$url][serialize($headers)] = array(
-            'headers' => $responseheaders,
-            'body' => $body,
-            'code' => $code,
-            'isfile' => $isfile
-        );
+        self::$requestMap[$urlbase] = $dir;
     }
 }
+
+class Internet_Exception extends Exception {}
 
 class Internet_Adapter extends PEAR2_HTTP_Request_Adapter
 {
@@ -38,6 +34,7 @@ class Internet_Adapter extends PEAR2_HTTP_Request_Adapter
     {
         $this->internet = $internet;
     }
+
     /**
      * Send the request
      *
@@ -50,26 +47,32 @@ class Internet_Adapter extends PEAR2_HTTP_Request_Adapter
      */
     public function sendRequest() 
     {
+        $uri = $this->uri->url;
+        $actualfile = false;
+        foreach (Internet::$requestMap as $urlbase => $dir) {
+            if (strpos($urlbase, str_replace('\\', '/', $uri)) === 0) {
+                $actualfile = $dir . DIRECTORY_SEPARATOR . substr($urlbase, strlen($uri)+1);
+                break;
+            }
+        }
+        if (!$actualfile) {
+            throw new Internet_Exception('URL ' . $uri . ' is not in the request map, setup is needed');
+        }
+        if (!file_exists($actualfile)) {
+            $details['code'] = '404';
+            $body = '';
+        } else {
+            $body = file_get_contents($actualfile);
+            $details['code'] = '200';
+        }
         // $this->verb is GET/POST/etc.
         // $this->uri is PEAR2_HTTP_Request_Uri
         // $this->headers is array of headers
         // $this->body is request body
-        if (isset($this->internet->requestMap[$this->verb][$this->uri->url][serialize($this->headers)])) {
-            $info = $this->internet->requestMap[$this->verb][$this->uri->url][serialize($this->headers)];
-            $body = $info['isfile'] ? file_get_contents($info['body']) : $info['body'];
-            $headers = $info['headers'];
-            $details['code'] = $info['code'];
-        } else {
-            $details['code'] = '404';
-            $body = '';
-        }
 
         $details = $this->uri->toArray();
-
         $details['httpVersion'] = 'HTTP/1.1';
-
         $cookies = array();
-
 
         return new PEAR2_HTTP_Request_Response($details, $body, $headers, $cookies);
     }	   
