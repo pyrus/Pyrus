6ab1d3d6c800eed5766fda4cff4824840fde5dc6
Greg Beaver <greg@chiaraquartet.net>
2009-05-20 17:59:10 +0000

    modify packages to allow them to install without file conflicts
    git-svn-id: http://svn.php.net/repository/pear2/Pyrus/trunk@285878 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/Mocks/Internet/installer.prepare.dep.versionconflict/make.php
+++ tests/Mocks/Internet/installer.prepare.dep.versionconflict/make.php
@@ -38,6 +38,10 @@ $scs->saveChannel();
 
 $pf = new PEAR2_Pyrus_PackageFile_v2;
 
+for ($i = 1; $i <= 5; $i++) {
+    file_put_contents(__DIR__ . "/glooby$i", 'hi');
+}
+
 $pf->name = 'P1';
 $pf->channel = 'pear2.php.net';
 $pf->summary = 'testing';
@@ -46,42 +50,47 @@ $pf->stability['release'] = 'stable';
 $pf->description = 'hi description';
 $pf->notes = 'my notes';
 $pf->maintainer['cellog']->role('lead')->email('cellog@php.net')->active('yes')->name('Greg Beaver');
-$pf->files['glooby'] = array('role' => 'php');
 
 $pf->setPackagefile(__DIR__ . '/package.xml');
 $save = clone $pf;
 
 $pf->dependencies['required']->package['pear2.php.net/P2']->min('1.2.0');
+$pf->files['glooby1'] =  array('role' => 'php');
 
 $p2_3 = clone $save;
 $p2_3->name = 'P2';
 $p2_3->version['release'] = '1.2.3';
 $p2_3->dependencies['required']->package['pear2.php.net/P3']->save();
 $p2_3->dependencies['required']->package['pear2.php.net/P5']->save();
+$p2_3->files['glooby2'] =  array('role' => 'php');
 
 $p2_2 = clone $save;
 $p2_2->name = 'P2';
 $p2_2->version['release'] = '1.2.2';
 $p2_2->dependencies['required']->package['pear2.php.net/P3']->save();
+$p2_2->files['glooby2'] =  array('role' => 'php');
 
 
 $p2_0 = clone $save;
 $p2_0->name = 'P2';
 $p2_0->version['release'] = '0.9.0';
 $p2_0->stability['release'] = 'beta';
+$p2_0->files['glooby2'] =  array('role' => 'php');
 
 $p3 = clone $save;
 $p3->name = 'P3';
+$p3->files['glooby3'] =  array('role' => 'php');
 
 $p4 = clone $save;
 $p4->name = 'P4';
 $p4->dependencies['required']->package['pear2.php.net/P2']->min('1.2.0')->exclude('1.2.3');
+$p4->files['glooby4'] =  array('role' => 'php');
 
 $p5 = $save;
 $p5->name = 'P5';
 $p5->stability['release'] = 'beta';
 $p5->version['release'] = '0.9.0';
-
+$p5->files['glooby5'] =  array('role' => 'php');
 
 file_put_contents(__DIR__ . '/package.xml', $pf);
 
@@ -136,6 +145,10 @@ $scs->saveRelease($package5, 'cellog');
 // clean up
 unlink(dirname(__DIR__) . '/pearconfig.xml');
 unlink(dirname(__DIR__) . '/.config');
+for ($i = 1; $i <= 5; $i++) {
+    unlink(__DIR__ . "/glooby$i");
+}
+unlink(__DIR__ . '/package.xml');
 foreach (new DirectoryIterator(dirname(__DIR__) . '/.configsnapshots') as $file) {
     if ($file->isDot()) {
         continue;
