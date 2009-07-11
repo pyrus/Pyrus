75c31276ab5b751fad663adbe069013503e110ea
Brett Bieber <brett.bieber@gmail.com>
2009-07-11 12:53:25 +0000

    Also depend on HTTP_Request
    git-svn-id: http://svn.php.net/repository/pear2/Pyrus/trunk@286225 c90b9560-bf6c-de11-be94-00142212c4b1

--- packagexmlsetup.php
+++ packagexmlsetup.php
@@ -2,8 +2,10 @@
 $package->dependencies['required']->package['pear2.php.net/PEAR2_Autoload']->save();
 $package->dependencies['required']->package['pear2.php.net/PEAR2_Exception']->save();
 $package->dependencies['required']->package['pear2.php.net/PEAR2_MultiErrors']->save();
+$package->dependencies['required']->package['pear2.php.net/PEAR2_HTTP_Request']->save();
 
 $compatible->dependencies['required']->package['pear2.php.net/PEAR2_Autoload']->save();
 $compatible->dependencies['required']->package['pear2.php.net/PEAR2_Exception']->save();
 $compatible->dependencies['required']->package['pear2.php.net/PEAR2_MultiErrors']->save();
+$compatible->dependencies['required']->package['pear2.php.net/PEAR2_HTTP_Request']->save();
 ?>
