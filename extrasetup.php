9a5d1546eb0c49ef256323b020e65ff96b8fa210
Brett Bieber <brett.bieber@gmail.com>
2009-07-12 14:37:02 +0000

    Removed unused variables
    git-svn-id: http://svn.php.net/repository/pear2/Pyrus/trunk@286233 c90b9560-bf6c-de11-be94-00142212c4b1

--- extrasetup.php
+++ extrasetup.php
@@ -2,8 +2,6 @@
 /**
  * This file generates the pyrus.phar file and PEAR2 package for Pyrus.
  */
-$rp = __DIR__ . '/../HTTP_Request/src/HTTP';
-$cc = __DIR__ . '/../sandbox/Console_CommandLine/src/Console';
 $extrafiles = array(
     new \pear2\Pyrus\Package(__DIR__ . '/../HTTP_Request/package.xml'),
     new \pear2\Pyrus\Package(__DIR__ . '/../sandbox/Console_CommandLine/package.xml'),
