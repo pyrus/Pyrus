56280edd0208ca102b517a349ef249fc02f0fd5b
Greg Beaver <greg@chiaraquartet.net>
2009-07-20 23:10:26 +0000

    house cleaning: set proper svn:ignore attributes
    git-svn-id: http://svn.php.net/repository/pear2/Pyrus/trunk@286280 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/ignorexdebug.php
+++ tests/ignorexdebug.php
@@ -9,6 +9,6 @@ if (!isset($_SERVER['argv'],$_SERVER['argv'][1])
 
 for ($i=1; $i<$_SERVER['argc']; $i++) {
     if (is_dir($_SERVER['argv'][$i])) {
-        exec('svn propset svn:ignore *.xdebug '.$_SERVER['argv'][$i]);
+        exec('svn propset svn:ignore -F ' . __DIR__ . '/.cvsignore ' . $_SERVER['argv'][$i]);
     }
 }
