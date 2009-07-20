fc578c60e7ed138bda8e9c8315297797afdd1d21
Greg Beaver <greg@chiaraquartet.net>
2009-07-20 22:16:30 +0000

    update to new internet maker, re-generate the packages and add intermediate releases so existing test will pass with new dep stuff
    git-svn-id: http://svn.php.net/repository/pear2/Pyrus/trunk@286278 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/Mocks/Internet/installer.prepare.depconflict2/make.php
+++ tests/Mocks/Internet/installer.prepare.depconflict2/make.php
@@ -8,97 +8,28 @@
  * to test composite dep failure
  */
 
-require __DIR__ . '/../../../../../autoload.php';
+require __DIR__ . '/../InternetMaker.php';
 
-set_include_path(__DIR__);
-$c = \pear2\Pyrus\Config::singleton(dirname(__DIR__), dirname(__DIR__) . '/pearconfig.xml');
-$c->bin_dir = __DIR__ . '/bin';
-restore_include_path();
-$c->saveConfig();
+$maker = new InternetMaker(__DIR__);
 
-$chan = new pear2\SimpleChannelServer\Channel('pear2.php.net', 'unit test channel');
-$scs = new pear2\SimpleChannelServer\Main($chan, __DIR__, dirname(__DIR__) . '/PEAR2');
-
-$scs->saveChannel();
-
-$pf = new \pear2\Pyrus\PackageFile\v2;
-
-for ($i = 1; $i <= 3; $i++) {
-    file_put_contents(__DIR__ . "/glooby$i", 'hi');
-}
-
-$pf->name = 'P1';
-$pf->channel = 'pear2.php.net';
-$pf->summary = 'testing';
-$pf->version['release'] = '1.0.0';
-$pf->stability['release'] = 'stable';
-$pf->description = 'hi description';
-$pf->notes = 'my notes';
-$pf->maintainer['cellog']->role('lead')->email('cellog@php.net')->active('yes')->name('Greg Beaver');
-
-$pf->setPackagefile(__DIR__ . '/package.xml');
-$save = clone $pf;
-
+$pf = $maker->getPassablePf('P1', '1.0.0');
 $pf->dependencies['required']->package['pear2.php.net/P2']->recommended('1.2.0');
 $pf->files['glooby1'] =  array('role' => 'php');
+$maker->makePackage($pf);
 
-$p2 = clone $save;
-$p2->name = 'P2';
-$p2->version['release'] = '1.2.1';
-$p2->stability['release'] = 'stable';
-$p2->files['glooby2'] =  array('role' => 'php');
+$pf = $maker->getPassablePf('P2', '1.1.0');
+$pf->files['glooby2'] =  array('role' => 'php');
+$maker->makePackage($pf);
 
-$p3 = clone $save;
-$p3->name = 'P3';
-$p3->files['glooby3'] =  array('role' => 'php');
-$p3->dependencies['required']->package['pear2.php.net/P2']->recommended('1.1.0');
-$p3->files['glooby3'] =  array('role' => 'php');
+$pf = $maker->getPassablePf('P2', '1.2.0');
+$pf->files['glooby2'] =  array('role' => 'php');
+$maker->makePackage($pf);
 
-file_put_contents(__DIR__ . '/package.xml', $pf);
+$pf = $maker->getPassablePf('P2', '1.2.1');
+$pf->files['glooby2'] =  array('role' => 'php');
+$maker->makePackage($pf);
 
-$package1 = new \pear2\Pyrus\Package(false);
-$xmlcontainer = new \pear2\Pyrus\PackageFile($pf);
-$xml = new \pear2\Pyrus\Package\Xml(__DIR__ . '/package.xml', $package1, $xmlcontainer);
-$package1->setInternalPackage($xml);
-$package1->archivefile = __DIR__ . '/package.xml';
-$scs->saveRelease($package1, 'cellog');
-
-$package2 = new \pear2\Pyrus\Package(false);
-$xmlcontainer = new \pear2\Pyrus\PackageFile($p2);
-$xml = new \pear2\Pyrus\Package\Xml(__DIR__ . '/package.xml', $package2, $xmlcontainer);
-$package2->setInternalPackage($xml);
-file_put_contents(__DIR__ . '/package.xml', $p2);
-$package2->archivefile = __DIR__ . '/package.xml';
-$scs->saveRelease($package2, 'cellog');
-
-$package3 = new \pear2\Pyrus\Package(false);
-$xmlcontainer = new \pear2\Pyrus\PackageFile($p3);
-$xml = new \pear2\Pyrus\Package\Xml(__DIR__ . '/package.xml', $package3, $xmlcontainer);
-$package3->setInternalPackage($xml);
-file_put_contents(__DIR__ . '/package.xml', $p3);
-$package3->archivefile = __DIR__ . '/package.xml';
-$scs->saveRelease($package3, 'cellog');
-
-// clean up
-unlink(dirname(__DIR__) . '/pearconfig.xml');
-unlink(dirname(__DIR__) . '/.config');
-for ($i = 1; $i <= 3; $i++) {
-    unlink(__DIR__ . "/glooby$i");
-}
-unlink(__DIR__ . '/package.xml');
-foreach (new DirectoryIterator(dirname(__DIR__) . '/.configsnapshots') as $file) {
-    if ($file->isDot()) {
-        continue;
-    }
-    unlink($file->getPathName());
-}
-$dir = dirname(__DIR__) . '/.xmlregistry';
-include __DIR__ . '/../../../clean.php.inc';
-$dir = dirname(__DIR__) . '/.configsnapshots';
-include __DIR__ . '/../../../clean.php.inc';
-unlink(dirname(__DIR__) . '/.pear2registry');
-$dir = dirname(__DIR__) . '/PEAR2/.xmlregistry';
-include __DIR__ . '/../../../clean.php.inc';
-unlink(dirname(__DIR__) . '/PEAR2/.pear2registry');
-rmdir(dirname(__DIR__) . '/PEAR2/temp');
-rmdir(dirname(__DIR__) . '/temp');
+$pf = $maker->getPassablePf('P3', '1.0.0');
+$pf->dependencies['required']->package['pear2.php.net/P2']->recommended('1.1.0');
+$pf->files['glooby3'] =  array('role' => 'php');
+$maker->makePackage($pf);
