c462614d396565e7ecb019804a416d1a48e4a270
Helgi Þormar Þorbjörnsson <helgi@php.net>
2009-12-23 21:32:05 +0000

    Spacing
    git-svn-id: http://svn.php.net/repository/pear2/Pyrus/trunk@292564 c90b9560-bf6c-de11-be94-00142212c4b1

--- stub.php
+++ stub.php
@@ -7,12 +7,14 @@ if (version_compare(phpversion(), '5.3.1', '<')) {
         exit -1;
     }
 }
+
 foreach (array('phar', 'spl', 'pcre', 'simplexml') as $ext) {
     if (!extension_loaded($ext)) {
         echo 'Extension ', $ext, " is required\n";
         exit -1;
     }
 }
+
 try {
     Phar::mapPhar();
 } catch (Exception $e) {
@@ -20,6 +22,7 @@ try {
     echo $e->getMessage(), "\n";
     exit -1;
 }
+
 function pyrus_autoload($class)
 {
     $class = str_replace(array('_','\\'), '/', $class);
@@ -27,11 +30,13 @@ function pyrus_autoload($class)
         include 'phar://' . __FILE__ . '/PEAR2_Pyrus-@PACKAGE_VERSION@/php/' . $class . '.php';
     }
 }
+
 spl_autoload_register("pyrus_autoload");
 $phar = new Phar(__FILE__);
 $sig = $phar->getSignature();
 define('PYRUS_SIG', $sig['hash']);
 define('PYRUS_SIGTYPE', $sig['hash_type']);
+
 $frontend = new \pear2\Pyrus\ScriptFrontend\Commands;
 @array_shift($_SERVER['argv']);
 $frontend->run($_SERVER['argv']);
