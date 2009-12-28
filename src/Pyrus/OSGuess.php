7e710b5fe4fc02fd5270c617c7b9724a677c4f03
Brett Bieber <brett.bieber@gmail.com>
2009-12-28 20:36:36 +0000

    Remove PHP License references - the switch from PHP to New BSD was approved in the original code from PEAR.
    git-svn-id: http://svn.php.net/repository/pear2/Pyrus/trunk@292727 c90b9560-bf6c-de11-be94-00142212c4b1

--- src/Pyrus/OSGuess.php
+++ src/Pyrus/OSGuess.php
@@ -2,20 +2,14 @@
 /**
  * The \pear2\Pyrus\OSGuess class
  *
- * PHP versions 4 and 5
+ * PHP version 5
  *
- * LICENSE: This source file is subject to version 3.0 of the PHP license
- * that is available through the world-wide-web at the following URI:
- * http://www.php.net/license/3_0.txt.  If you did not receive a copy of
- * the PHP License and are unable to obtain it through the web, please
- * send a note to license@php.net so we can mail you a copy immediately.
- *
- * @category   pear
- * @package    PEAR
+ * @category   PEAR2
+ * @package    PEAR2_Pyrus
  * @author     Stig Bakken <ssb@php.net>
  * @author     Gregory Beaver <cellog@php.net>
  * @copyright  1997-2007 The PHP Group
- * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD License
  * @version    CVS: $Id: Guess.php,v 1.25 2006/12/14 00:24:37 cellog Exp $
  * @link       http://pear.php.net/package/PEAR
  * @since      File available since Pyrus 0.1
