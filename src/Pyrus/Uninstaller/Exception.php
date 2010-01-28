f98d77a828ecb43c66643a63af5bef0c859c3b8f
Brett Bieber <brett.bieber@gmail.com>
2010-01-28 15:37:29 +0000

    Add missing exception file
    git-svn-id: http://svn.php.net/repository/pear2/Pyrus/trunk@294156 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ src/Pyrus/Uninstaller/Exception.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * \pear2\Pyrus\Uninstaller\Exception
+ *
+ * PHP version 5
+ *
+ * @category  PEAR2
+ * @package   PEAR2_Pyrus
+ * @author    Greg Beaver <cellog@php.net>
+ * @copyright 2010 The PEAR Group
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @version   SVN: $Id$
+ * @link      http://svn.php.net/viewvc/pear2/Pyrus/
+ */
+
+/**
+ * Exception class for Pyrus uninstaller
+ *
+ * @category  PEAR2
+ * @package   PEAR2_Pyrus
+ * @author    Greg Beaver <cellog@php.net>
+ * @copyright 2010 The PEAR Group
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @link      http://svn.php.net/viewvc/pear2/Pyrus/
+ */
+namespace pear2\Pyrus\Uninstaller;
+class Exception extends \pear2\Exception {}
