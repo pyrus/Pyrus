17d8b32ee6e3fbfd69a204cbb00af93a87365b0b
Brett Bieber <brett.bieber@gmail.com>
2010-05-23 19:37:44 +0000

    Minor simplification
    git-svn-id: http://svn.php.net/repository/pear2/Pyrus/trunk@299669 c90b9560-bf6c-de11-be94-00142212c4b1

--- extrasetup.php
+++ extrasetup.php
@@ -2,18 +2,14 @@
 /**
  * This file generates the pyrus.phar file and PEAR2 package for Pyrus.
  */
+$lib_path = __DIR__ . '/..';
 if (basename(__DIR__) == 'trunk') {
-    $extrafiles = array(
-        new \PEAR2\Pyrus\Package(__DIR__ . '/../../HTTP_Request/trunk/package.xml'),
-        new \PEAR2\Pyrus\Package(__DIR__ . '/../../sandbox/Console_CommandLine/trunk/package.xml'),
-        new \PEAR2\Pyrus\Package(__DIR__ . '/../../MultiErrors/trunk/package.xml'),
-        new \PEAR2\Pyrus\Package(__DIR__ . '/../../Exception/trunk/package.xml'),
-    );
-} else {
-    $extrafiles = array(
-        new \PEAR2\Pyrus\Package(__DIR__ . '/../HTTP_Request/package.xml'),
-        new \PEAR2\Pyrus\Package(__DIR__ . '/../sandbox/Console_CommandLine/package.xml'),
-        new \PEAR2\Pyrus\Package(__DIR__ . '/../MultiErrors/package.xml'),
-        new \PEAR2\Pyrus\Package(__DIR__ . '/../Exception/package.xml'),
-    );
+    $lib_path .= '/..';
 }
+
+$extrafiles = array(
+    new \PEAR2\Pyrus\Package($lib_path . '/HTTP_Request/trunk/package.xml'),
+    new \PEAR2\Pyrus\Package($lib_path . '/sandbox/Console_CommandLine/trunk/package.xml'),
+    new \PEAR2\Pyrus\Package($lib_path . '/MultiErrors/trunk/package.xml'),
+    new \PEAR2\Pyrus\Package($lib_path . '/Exception/trunk/package.xml'),
+);
