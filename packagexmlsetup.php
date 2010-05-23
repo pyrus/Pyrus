acd297669e697b1a74f54149784a486665f8dc5c
Brett Bieber <brett.bieber@gmail.com>
2010-05-23 19:37:08 +0000

    Add minimum versions to required dependencies. Add PEAR2_Console_CommandLine as a required dep.
    git-svn-id: http://svn.php.net/repository/pear2/Pyrus/trunk@299668 c90b9560-bf6c-de11-be94-00142212c4b1

--- packagexmlsetup.php
+++ packagexmlsetup.php
@@ -1,15 +1,17 @@
 <?php
 $package->dependencies['required']->php = '5.3.1RC1';
-$package->dependencies['required']->package['pear2.php.net/PEAR2_Autoload']->save();
-$package->dependencies['required']->package['pear2.php.net/PEAR2_Exception']->save();
-$package->dependencies['required']->package['pear2.php.net/PEAR2_MultiErrors']->save();
-$package->dependencies['required']->package['pear2.php.net/PEAR2_HTTP_Request']->save();
+$package->dependencies['required']->package['pear2.php.net/PEAR2_Autoload']->min('0.2.0');
+$package->dependencies['required']->package['pear2.php.net/PEAR2_Exception']->min('0.2.0');
+$package->dependencies['required']->package['pear2.php.net/PEAR2_MultiErrors']->min('0.2.0');
+$package->dependencies['required']->package['pear2.php.net/PEAR2_HTTP_Request']->min('0.2.0');
+$package->dependencies['required']->package['pear2.php.net/PEAR2_Console_CommandLine']->min('0.1.0');
 
 $compatible->dependencies['required']->php = '5.3.1RC1';
-$compatible->dependencies['required']->package['pear2.php.net/PEAR2_Autoload']->save();
-$compatible->dependencies['required']->package['pear2.php.net/PEAR2_Exception']->save();
-$compatible->dependencies['required']->package['pear2.php.net/PEAR2_MultiErrors']->save();
-$compatible->dependencies['required']->package['pear2.php.net/PEAR2_HTTP_Request']->save();
+$compatible->dependencies['required']->package['pear2.php.net/PEAR2_Autoload']->min('0.2.0');
+$compatible->dependencies['required']->package['pear2.php.net/PEAR2_Exception']->min('0.2.0');
+$compatible->dependencies['required']->package['pear2.php.net/PEAR2_MultiErrors']->min('0.2.0');
+$compatible->dependencies['required']->package['pear2.php.net/PEAR2_HTTP_Request']->min('0.2.0');
+$compatible->dependencies['required']->package['pear2.php.net/PEAR2_Console_CommandLine']->min('0.1.0');
 
 unset($package->files['tests/*']);
 
