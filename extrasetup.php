36c7984d45cf62e9c24d6ecef4e0b9f15df0178a
Brett Bieber <brett.bieber@gmail.com>
2010-05-26 13:55:52 +0000

    Don't include trunk dir if we're building from a checkout of pear2/all.
    git-svn-id: http://svn.php.net/repository/pear2/Pyrus/trunk@299787 c90b9560-bf6c-de11-be94-00142212c4b1

--- extrasetup.php
+++ extrasetup.php
@@ -3,13 +3,16 @@
  * This file generates the pyrus.phar file and PEAR2 package for Pyrus.
  */
 $lib_path = __DIR__ . '/..';
+$xml_path = 'package.xml';
+
 if (basename(__DIR__) == 'trunk') {
     $lib_path .= '/..';
+    $xml_path = 'trunk/package.xml';
 }
 
 $extrafiles = array(
-    new \PEAR2\Pyrus\Package($lib_path . '/HTTP_Request/trunk/package.xml'),
-    new \PEAR2\Pyrus\Package($lib_path . '/sandbox/Console_CommandLine/trunk/package.xml'),
-    new \PEAR2\Pyrus\Package($lib_path . '/MultiErrors/trunk/package.xml'),
-    new \PEAR2\Pyrus\Package($lib_path . '/Exception/trunk/package.xml'),
+    new \PEAR2\Pyrus\Package($lib_path . '/HTTP_Request/'.$xml_path),
+    new \PEAR2\Pyrus\Package($lib_path . '/sandbox/Console_CommandLine/'.$xml_path),
+    new \PEAR2\Pyrus\Package($lib_path . '/MultiErrors/'.$xml_path),
+    new \PEAR2\Pyrus\Package($lib_path . '/Exception/'.$xml_path),
 );
