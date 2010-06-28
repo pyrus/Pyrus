978022c6ef020f6274e39169b0df22d075f0ac2e
Brett Bieber <brett.bieber@gmail.com>
2010-06-28 19:22:54 +0000

    Add correct php_dir replacement task to the scripts/pyrus file so when it is executed it functions correctly.
    git-svn-id: http://svn.php.net/repository/pear2/Pyrus/trunk@300824 c90b9560-bf6c-de11-be94-00142212c4b1

--- packagexmlsetup.php
+++ packagexmlsetup.php
@@ -13,5 +13,9 @@ $compatible->dependencies['required']->package['pear2.php.net/PEAR2_MultiErrors'
 $compatible->dependencies['required']->package['pear2.php.net/PEAR2_HTTP_Request']->min('0.2.0');
 $compatible->dependencies['required']->package['pear2.php.net/PEAR2_Console_CommandLine']->min('0.1.0');
 
-unset($package->files['tests/*']);
+$package->files['scripts/pyrus'] = array(
+    'attribs' => array('role' => 'script'),
+    'tasks:replace' => array('attribs' =>
+                             array('from' => '@php_dir@', 'to' => 'php_dir', 'type' => 'pear-config'))
+);
 
