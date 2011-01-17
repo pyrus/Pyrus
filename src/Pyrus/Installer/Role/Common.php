18a96f943916c5040ae00667dcff10907a41999d
Warnar Boekkooi <warstar-web@gmx.net>
2011-01-17 09:59:39 +0100

    Make sure default roles are loaded when a custom role is added, fixed exception typo, fixed relative location path.

--- src/Pyrus/Installer/Role/Common.php
+++ src/Pyrus/Installer/Role/Common.php
@@ -183,8 +183,8 @@ class Common
                 // strip role from file path
                 // so php/Path/To/File.php becomes Path/To/File.php,
                 // data/package.xsd becomes package.xsd
-                $r = strtolower(str_replace('PEAR2\Pyrus\Installer\Role\\', '',
-                      get_class($this)));
+				$r = get_class($this);
+				$r = strtolower(substr($r, strrpos($r, '\\') + 1));
                 if ($r === 'php') {
                     if (strpos($newpath, 'src') === 0) {
                         $newpath = substr($newpath, 4);
