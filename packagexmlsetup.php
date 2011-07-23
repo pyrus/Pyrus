3283c98d63be9c6883f1c984a76b20cb88748e49
helgi <helgith@gmail.com>
2011-07-24 00:11:50 +0100

    Update requirements

--- packagexmlsetup.php
+++ packagexmlsetup.php
@@ -1,17 +1,17 @@
 <?php
 $package->dependencies['required']->php = '5.3.1RC1';
-$package->dependencies['required']->package['pear2.php.net/PEAR2_Autoload']->min('0.2.0');
+$package->dependencies['required']->package['pear2.php.net/PEAR2_Autoload']->min('0.2.4');
 $package->dependencies['required']->package['pear2.php.net/PEAR2_Exception']->min('0.2.0');
 $package->dependencies['required']->package['pear2.php.net/PEAR2_MultiErrors']->min('0.2.0');
 $package->dependencies['required']->package['pear2.php.net/PEAR2_HTTP_Request']->min('0.2.0');
-$package->dependencies['required']->package['pear2.php.net/PEAR2_Console_CommandLine']->min('0.1.0');
+$package->dependencies['required']->package['pear2.php.net/PEAR2_Console_CommandLine']->min('0.2.0');
 
 $compatible->dependencies['required']->php = '5.3.1RC1';
-$compatible->dependencies['required']->package['pear2.php.net/PEAR2_Autoload']->min('0.2.0');
+$compatible->dependencies['required']->package['pear2.php.net/PEAR2_Autoload']->min('0.2.4');
 $compatible->dependencies['required']->package['pear2.php.net/PEAR2_Exception']->min('0.2.0');
 $compatible->dependencies['required']->package['pear2.php.net/PEAR2_MultiErrors']->min('0.2.0');
 $compatible->dependencies['required']->package['pear2.php.net/PEAR2_HTTP_Request']->min('0.2.0');
-$compatible->dependencies['required']->package['pear2.php.net/PEAR2_Console_CommandLine']->min('0.1.0');
+$compatible->dependencies['required']->package['pear2.php.net/PEAR2_Console_CommandLine']->min('0.2.0');
 
 $package->files['scripts/pyrus'] = array(
     'attribs' => array('role' => 'script'),
