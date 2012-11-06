pyrus/Pyrus

commit ca074ae5003690b45c8e49a9ba469431ce1dc5e6

Use return value of custom command to determine exit code
Currently the exit code is always 0 for external (plugin) commands.
This causes problems for commands like run-phpt which should return 1
when tests fail. The return value of the plugin command will determine
what exit code to use.

Closes #91

src/Pyrus/ScriptFrontend/Commands.php
@@ -310,7 +310,7 @@ class Commands implements \Pyrus\LogInterface
                     }
                 } else {
                     $class = new $info['class'];
-                    $class->{$info['function']}($this, $result->command->args, $result->command->options);
+                    exit((int)!$class->{$info['function']}($this, $result->command->args, $result->command->options));
                 }
             } else {
                 $this->help(array('command' => isset($args[0]) ? $args[0] : null));

commit c638fdd1f898ccf713bbec57d9c0a8a5e34e9bcb

Bugfix: fix for regression introduced in ca074ae Change: ->run() should (!) always return an exit code, we exit() in the wrapper
Related ticket: #105

scripts/pyrus
@@ -50,4 +50,8 @@ require_once $autoload;
 
 $frontend = new \Pyrus\ScriptFrontend\Commands;
 @array_shift($_SERVER['argv']);
-$frontend->run($_SERVER['argv']);
+$exitCode = $frontend->run($_SERVER['argv']);
+if (null === $exitCode || !is_numeric($exitCode)) {
+    $exitCode = (int) $exitCode;
+}
+exit($exitCode);

src/Pyrus/ScriptFrontend/Commands.php
@@ -267,7 +267,7 @@ class Commands implements \Pyrus\LogInterface
      *
      * @param array $args An array of command line arguments.
      *
-     * @return void
+     * @return int
      */
     function run($args)
     {
@@ -310,7 +310,7 @@ class Commands implements \Pyrus\LogInterface
                     }
                 } else {
                     $class = new $info['class'];
-                    exit((int)!$class->{$info['function']}($this, $result->command->args, $result->command->options));
+                    return (int)!$class->{$info['function']}($this, $result->command->args, $result->command->options);
                 }
             } else {
                 $this->help(array('command' => isset($args[0]) ? $args[0] : null));